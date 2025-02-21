e7d30dea79183ebd40be55b817e443da732b89fb
Tshering00 <tshering.work1@gmail.com>
2025-02-21 16:24:24 +0545

    added database model

--- formApp/admin.py
+++ formApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from formApp.models import User
 # Register your models here.
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id','name', 'email', 'message')
+
+admin.site.register(User, UserAdmin)
--- formApp/views.py
+++ formApp/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render, redirect
 from formApp.form import ContactForm
-# Create your views here.
-
-# def index(request):
-#     return HttpResponse( "hello world")
+from formApp.models import User
 
 def home_view(request):
     return render(request, 'html/home.html')
@@ -12,11 +9,22 @@ def contact_view(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
-            form.send_email()
-            # name = form.cleaned_data["name"]
-            # email = form.cleaned_data["email"]
-            # message = form.cleaned_data["message"]
-            # print(f"Message is sent from {name} ({email}): {message}")
+            # form.send_email()
+            name = form.cleaned_data["name"]
+            email = form.cleaned_data["email"]
+            message = form.cleaned_data["message"]
+            
+            #save data into database
+            #method 1
+            # from .models import User
+            # User.objects.create(name=name, email=email, message=message)
+            
+            #method 2
+            data = User( name = name, email = email, message = message)
+            data.save()
+            
+            #to update data set the id then do
+            
             return redirect('success')
     else:
         form = ContactForm()
